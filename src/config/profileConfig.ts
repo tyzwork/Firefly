58842307ad59cad749bd5077a142242dc8f4fb6f
tyzwork <tyzwork@outlook.com>
2026-08-15 20:01:38 +0800

    Update bio in profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "tyz",
 
 	// 个人签名
-	bio: "Hello, I'm tyx.",
+	bio: "Hello, I'm tyz.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
